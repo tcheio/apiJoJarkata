f634043356b6c69d35eba896cc09b978413baea4
tcheio <thomas.cheio0105@gmail.com>
2024-04-18 09:57:03 +0200

    init

---
+++ src/main/java/com/example/jojakartaapi/JoJakartaApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.jojakartaapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JoJakartaApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JoJakartaApiApplication.class, args);
+    }
+
+}
