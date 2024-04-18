e5b857068d554f1a0c48976726036162bb673791
tcheio <thomas.cheio0105@gmail.com>
2024-04-18 11:08:57 +0200

    Ajout dépendance et connexion BDD

--- src/main/java/com/example/jojakartaapi/JoJakartaApiApplication.java
+++ src/main/java/com/example/jojakartaapi/JoJakartaApiApplication.java
@@ -2,10 +2,17 @@ package com.example.jojakartaapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class JoJakartaApiApplication {
 
+    @RequestMapping("/")
+    String home(){
+        return "Hello World";
+    }
     public static void main(String[] args) {
         SpringApplication.run(JoJakartaApiApplication.class, args);
     }
