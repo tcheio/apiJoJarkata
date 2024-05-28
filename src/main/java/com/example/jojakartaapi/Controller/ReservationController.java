232cf2dbc8e61b4b67b6376f8d28c125e2d22be3
tcheio <thomas.cheio0105@gmail.com>
2024-05-28 22:22:16 +0200

    Réservation presque fonctionnel (date jamais au bon format pour être utilisé)

--- src/main/java/com/example/jojakartaapi/Controller/ReservationController.java
+++ src/main/java/com/example/jojakartaapi/Controller/ReservationController.java
@@ -8,7 +8,9 @@ import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.Calendar;
 import java.util.Date;
+import java.util.GregorianCalendar;
 
 @RestController
 @RequestMapping("/api/reservations")
@@ -23,18 +25,22 @@ public class ReservationController {
     @PostMapping
     public ResponseEntity<String> createReservation(@RequestParam Long visiteurId,
                                                     @RequestParam Long epreuveId,
-                                                    @RequestParam int nbPlaces,
-                                                    @RequestParam @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME) Date date) {
+                                                    @RequestParam int nbPlaces){
+        Date date = new GregorianCalendar(2024, Calendar.MAY, 1, 10, 0).getTime();
+        System.out.println("Received request with visiteurId: " + visiteurId + ", epreuveId: " + epreuveId + ", nbPlaces: " + nbPlaces + ", date: " + date);
+
         Visiteur visiteur = visiteurService.getVisiteurById(visiteurId);
         if (visiteur == null) {
-            return ResponseEntity.badRequest().body("Visiteur not found");
+            return ResponseEntity.badRequest().body("Visiteur pas trouvé");
         }
 
         boolean reservationCreated = reservationService.createReservation(visiteur, epreuveId, nbPlaces, date);
         if (reservationCreated) {
-            return ResponseEntity.ok("Reservation created successfully");
+            return ResponseEntity.ok("Reservation créée");
         } else {
-            return ResponseEntity.badRequest().body("Reservation could not be created due to constraints");
+            return ResponseEntity.badRequest().body("Reservation ne peut pas être créée");
         }
     }
+
 }
+
